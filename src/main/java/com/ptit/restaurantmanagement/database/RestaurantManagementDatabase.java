5c04b86d353ccb721557f967d82684f0e5e9f704
Van Linh Nguyen <linhvnguyen9@gmail.com>
2019-10-28 14:36:50 +0700

    Add createDatabase to RestaurantManagementDatabase class

--- src/main/java/com/ptit/restaurantmanagement/database/RestaurantManagementDatabase.java
+++ src/main/java/com/ptit/restaurantmanagement/database/RestaurantManagementDatabase.java
@@ -3,16 +3,26 @@ package com.ptit.restaurantmanagement.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class RestaurantManagementDatabase {
-    public static final String URL = "jdbc:mysql://localhost:3306/testdb";
-    public static final String USER = "testuser";
-    public static final String PASS = "testpass";
+    public static final String BASEURL = "jdbc:mysql://localhost:3306/";
+    public static final String DATABASENAME = "testDb";
+    public static final String USER = "root";
+    public static final String PASS = "";
+
+    public static void createDatabase(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        String createDatabase = "CREATE DATABASE IF NOT EXISTS " + DATABASENAME;
+        statement.execute(createDatabase);
+    }
 
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(URL, USER, PASS);
+            Connection connection = DriverManager.getConnection(BASEURL, USER, PASS);
+            createDatabase(connection);
+            return connection;
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
