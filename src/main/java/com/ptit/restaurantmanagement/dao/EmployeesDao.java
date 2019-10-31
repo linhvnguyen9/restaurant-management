a09c3275fc76d9def39d884985b3d8ed29bdebf0
Nguyen Duc Anh <ducanhslvp99@gmail.com>
2019-10-31 07:37:48 +0700

    Add edit and update employee in EmployeesDao

--- src/main/java/com/ptit/restaurantmanagement/dao/EmployeesDao.java
+++ src/main/java/com/ptit/restaurantmanagement/dao/EmployeesDao.java
@@ -3,7 +3,7 @@ package com.ptit.restaurantmanagement.dao;
 
 import com.ptit.restaurantmanagement.database.RestaurantManagementDatabase;
 import com.ptit.restaurantmanagement.domain.model.Employee;
-import com.ptit.restaurantmanagement.domain.model.Person;
+import com.ptit.restaurantmanagement.domain.model.EmployeeType;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -12,17 +12,18 @@ import java.util.Date;
 
 public class EmployeesDao {
     private Statement statement;
-    private Connection connection = RestaurantManagementDatabase.getConnection();
+    private Connection stament = RestaurantManagementDatabase.getConnection();
+    ArrayList<Employee> listEmployee = new ArrayList<>();
 
     public EmployeesDao() throws SQLException {
-         RestaurantManagementDatabase.createDatabase(connection);
-         RestaurantManagementDatabase.getConnection();
+        RestaurantManagementDatabase.createDatabase(stament);
+        RestaurantManagementDatabase.getConnection();
     }
 
     public int insertEmployee(Employee employee) throws SQLException {
         String createPerson = "INSERT INTO person VALUES (0, ?, ?, ?)";
 
-        PreparedStatement preparedStatement = connection.prepareStatement(createPerson, Statement.RETURN_GENERATED_KEYS);
+        PreparedStatement preparedStatement = stament.prepareStatement(createPerson, Statement.RETURN_GENERATED_KEYS);
         preparedStatement.setString(1, employee.getName());
 
         Date utilDate = employee.getDob().getTime();
@@ -43,7 +44,7 @@ public class EmployeesDao {
 
         String createEmployee = "INSERT INTO employee VALUES(?, ?, ?, ?)";
 
-        preparedStatement = connection.prepareStatement(createEmployee);
+        preparedStatement = stament.prepareStatement(createEmployee);
         preparedStatement.setInt(1, personId);
         preparedStatement.setString(2, employee.getEmployeeType().toString());
 
@@ -61,33 +62,103 @@ public class EmployeesDao {
         return personId;
     }
 
-//    public int updateEmployee(int id) {
-//        return connection.update(
-//                "UPDATE employee  SET name=?, dob=?, addr=?, type='?, where id = ? limit 1;",name,dob,addr,type,id);
-//    }
-     public ArrayList<Person> getListPersons(){
-        ArrayList<Person> list = new ArrayList<>();
-        String sql = "SELECT * FROM employee";
-        
+    public ArrayList<Employee> getListEmployee() {
+        String sql = "SELECT * FROM testdb.person,testdb.employee WHERE id_person = id_employee;";
+
         try {
-            PreparedStatement ps = connection.prepareStatement(sql);
+            PreparedStatement ps = stament.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){
-                Person s=new Person(sql);
-                s.setId(rs.getInt("ID"));
+            while (rs.next()) {
+                Employee s = new Employee(sql);
+                s.setId(rs.getInt("id_employee"));
                 s.setName(rs.getString("name"));
-                
+
                 Calendar dob = Calendar.getInstance();
                 dob.setTime(rs.getDate("dob"));
-                
+
                 s.setDob(dob);
-                s.setAddress(rs.getString("address"));                              
-                list.add(s);
+                s.setAddress(rs.getString("addr"));
+                if (rs.getString("type").equals("MANAGER"))
+                    s.setEmployeeType(EmployeeType.MANAGER);
+                else
+                    s.setEmployeeType(EmployeeType.NORMAL);
+                s.setManagerId(rs.getInt("id_manager"));
+                s.setBaseSalary(rs.getDouble("salary"));
+                listEmployee.add(s);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
-        return list;
+        //print out listEmployee
+        for (int i = 0; i < listEmployee.size(); i++)
+            System.out.println(listEmployee.get(i).toString());
+        return listEmployee;
+    }
+
+    public void updateEmployee(Employee employee, int id) throws SQLException {
+        //update employee
+
+        String updateEmployees = "UPDATE employee SET type=?,salary=? WHERE id_employee=?;";
+        PreparedStatement pstmt = stament.prepareStatement(updateEmployees);
+
+        pstmt.setString(1, employee.getEmployeeType().toString());
+
+        //pstmt.setInt(2, employee.getManagerId());
+        pstmt.setDouble(2, employee.getBaseSalary());
+        pstmt.setInt(3, id);
+        System.out.println(pstmt.toString());
+        pstmt.executeUpdate();
+
+        //update person
+        String updatePerson = "UPDATE person SET name=?, dob=?, addr=? WHERE id_person=?;";
+        PreparedStatement pstmt2 = stament.prepareStatement(updatePerson);
+
+        pstmt2.setString(1, employee.getName());
+
+        Date utilDate = employee.getDob().getTime();
+        java.sql.Date date = new java.sql.Date(utilDate.getTime());
+
+        pstmt2.setDate(2, date);
+        pstmt2.setString(3, employee.getAddress());
+        pstmt2.setInt(4, id);
+
+        System.out.println(pstmt2.toString());
+        pstmt2.executeUpdate();
+
+
+
+        boolean autoCommit = stament.getAutoCommit();
+        try {
+            stament.setAutoCommit(false);
+
+            stament.commit();
+        } catch (SQLException exc) {
+            stament.rollback();
+        } finally {
+            stament.setAutoCommit(autoCommit);
+        }
+    }
+
+    public void deleteEmployee(int id) throws SQLException {
+        String deleteEmployee = "delete from employee where id_employee=?;";
+        PreparedStatement pstmEmployee = stament.prepareStatement(deleteEmployee);
+        pstmEmployee.setInt(1, id);
+        pstmEmployee.executeUpdate();
+
+        String deletePerson = "delete from person where id_person=?;";
+        PreparedStatement pstmtPerson = stament.prepareStatement(deletePerson);
+        pstmtPerson.setInt(1, id);
+        pstmtPerson.executeUpdate();
+
+
+
+
+    }
+
+
+    public void searchListEmployee(String name) {
+        for (int i = 0; i < listEmployee.size(); i++)
+            if (listEmployee.get(i).getName().equals(name))
+                System.out.println(listEmployee.get(i).toString());
     }
 }
